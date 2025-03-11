c9f2bbb0a34d6d96da2953f438107a1fc78b12c9
adamchmski <adamchmski@gmail.com>
2025-03-11 14:05:21 -0700

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./App.css";
+import StickyContainer from "./components/StickyContainer";
+
+function App() {
+  return <StickyContainer />;
+}
+
+export default App;
---
+++ src/components/StickyContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function StickyContainer() {
+  return <div>hello this is container</div>;
+}
+
+export default StickyContainer;
