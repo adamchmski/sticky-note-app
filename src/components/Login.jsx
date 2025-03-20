6573ee504354803a2e3c6406d69ce3ce5eea4f8f
adamchmski <adamchmski@gmail.com>
2025-03-20 13:13:27 -0700

    have a basic login form that just logs to console on submit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,9 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+// import Input from "../components/Input";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -24,6 +25,28 @@ export default function BasicModal() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [nameValue, setNameValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const nameInputHandler = (e) => {
+    setNameValue(e.target.value);
+  };
+
+  const emailInputHandler = (e) => {
+    setEmailValue(e.target.value);
+  };
+
+  const passwordInputHandler = (e) => {
+    setPasswordValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(nameValue);
+    console.log(emailValue);
+    console.log(passwordValue);
+  };
 
   return (
     <div>
@@ -43,9 +66,36 @@ export default function BasicModal() {
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Login
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            <input type="text"></input>
-          </Typography>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name:</label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              onChange={nameInputHandler}
+              value={nameValue}
+            ></input>
+            <br />
+            <label htmlFor="email">Email:</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              onChange={emailInputHandler}
+              value={emailValue}
+            ></input>
+            <br />
+            <label htmlFor="password">Password:</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              onChange={passwordInputHandler}
+              value={passwordValue}
+            ></input>
+            <br />
+            <button type="submit">Submit</button>
+          </form>
         </Box>
       </Modal>
     </div>
